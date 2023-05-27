0c25cf8dcd9a54dd36f5325ca8a978bdffcf0548
Chris Bentivenga <chris@chrisbenti.com>
2023-05-26 17:44:15 -0700

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import { getCurrentSeasons } from "./SKAHL/seasons";
+import { getCurrentSeasons } from './SKAHL/seasons';
 
 import * as fs from 'fs';
 
-const main = async () => {
-    let seasons = await getCurrentSeasons();
-    let teams = (await Promise.all(seasons.map(s => s.getTeams()))).flat();
-    teams = teams.filter(t => t.name.indexOf('Frost Giants') >= 0);
-    console.log(await teams[0].writeICS());
+async function main(): Promise<void> {
+    const seasons = await getCurrentSeasons();
+    let teams = (await Promise.all(seasons.map(async (s) => await s.getTeams()))).flat();
+    teams = teams.filter((t) => t.name.includes('Frost Giants'));
+    await teams[0].writeICS();
 }
 
-if (fs.existsSync('output')){
+if (fs.existsSync('output')) {
     fs.rmSync('output', { recursive: true, force: true });
-
 }
 fs.mkdirSync('output');
 
-main();
+main().catch((e) => {
+    throw e;
+});
